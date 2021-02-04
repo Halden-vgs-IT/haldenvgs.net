c6379fa7682d5401b467e40079ddad40d56987ed
Takotzo <71129486+Takotzo@users.noreply.github.com>
2021-02-04 08:06:16 +0100

    ver 1.1
    Oppdatert hjemmeside og elevside.
    NB! ikke inkluder header

--- index.php
+++ index.php
@@ -29,24 +29,24 @@
 <body>
 <header>
     <div class="box">
-        <h1 class="heading">Velkommen til IT</h1>
+        <h1 class="heading">IT på Halden VGS</h1>
         <p class="par">Herfra kan man se og komme seg til de forskjellige IT-klassene på Porsnes sine nettsider</p>
     </div>
 </header>
-<main>
-    <ul class="list">
-        <li>
-            <div class="boxit">
-
-            </div>
-        </li>
-        <li>
-            <div class="boxit">
-
-            </div>
-        </li>
-    </ul>
-</main>
+    <main class="flexbox-col">
+        <ul class="wide">
+            <li>
+                <div class="boxit">
+                    <p class="klasse">IT 2 - 2020/2021</p>
+                    <div class="ehh flexbox-space">
+                        <a><button class="knapp">IT 2 fellesside *ikke satt opp</button></a>
+                        <a href="elever.php"><button class="knapp">Elever</button></a>
+                        <a href="https://databit.haldenvgs.net"><button class="knapp">Lærer: Gunnar Kopperud</button></a>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </main>
 </body>
 <script src="javascript/script.js"></script>
 </html>
